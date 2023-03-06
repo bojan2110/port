b9d17209793049cf3c354abbf3e1002003e14ef4
bojan2110 <bojan.2110@gmail.com>
2023-03-06 18:16:34 +0100

    First version. Visual cues and data donation progress indication.

--- src/framework/processing/py/port/script.py
+++ src/framework/processing/py/port/script.py
@@ -14,6 +14,8 @@ def process(sessionId):
     # progress in %
     progress = 0
 
+    donatedFileFlag = [False, False, False, False, False]
+
     for _, platform in enumerate(platforms):
 
         data = None
@@ -21,7 +23,7 @@ def process(sessionId):
         counter = counter + 1
 
         while True:
-            promptFile = prompt_file(platform, "application/zip, text/plain")
+            promptFile = prompt_file(platform,counter, "application/zip, text/plain",donatedFileFlag)
             fileResult = yield render_donation_page(platform, counter, promptFile, progress)
             if fileResult.__type__ == 'PayloadString':
 
@@ -40,6 +42,8 @@ def process(sessionId):
                         list_with_df_with_chats = port.whatsapp.split_dataframe(df_with_chats, 5000)
 
                         data = list_with_df_with_chats
+
+                        donatedFileFlag[counter-1] = True
                         break
                 # If not enter retry flow
                 else:
@@ -93,8 +97,8 @@ def render_end_page():
 
 def render_donation_page(platform,counter, body, progress):
     header = props.PropsUIHeader(props.Translatable({
-        "en": 'Conversation ' + str(counter),
-        "nl": 'Conversation ' + str(counter)
+        "en": 'Conversation ' + str(counter) + ' of 5',
+        "nl": 'Conversation ' + str(counter) + ' of 5'
     }))
 
 
@@ -119,10 +123,34 @@ def retry_confirmation(platform):
     return props.PropsUIPromptConfirm(text, ok, cancel)
 
 
-def prompt_file(platform, extensions):
+def prompt_file(platform,counter, extensions,donatedFileFlag):
+    promptStringsSuccess = ['Hi, you are about to donate your first whatsapp file. ',
+    f'You have successfully donated your first file. It\'s time for the second one.',
+    'Nice, your second upload was successful. Let\'s move to the third file.',
+    'Great, we got that. You have two files left. Expecting your fourth whatsapp now.',
+    'Fourth file done! We are almost there, you are about to donate your last whatsapp file. ']
+
+    # only valid from the second round.
+    promptStringNoSuscess = ['Ups, no file was donated in the previous step (File 1). Want to try from stratch again?',
+    'Ups, no file was donated in the previous step (File 2). Want to try from stratch again?',
+    'Ups, no file was donated in the previous step (File 3). Want to try from stratch again?',
+    'Ups, no file was donated in the previous step (File 4). Want to try from stratch again?']
+
+    descriptionString = ''
+
+    print('prompt_file counter: ', counter)
+    print('prompt_file promptStrings: ', promptStringsSuccess[counter-1])
+    print('prompt_file donatedFileFlag: ', donatedFileFlag)
+
+    if(counter >=2 and donatedFileFlag[counter-2] == False):
+        descriptionString = promptStringNoSuscess[counter-2]
+    else:
+        descriptionString = promptStringsSuccess[counter-1]
+
+
     description = props.Translatable({
         "en": f"Please follow the download instructions and choose the file that you stored on your device. Click “Skip” at the right bottom, if you do not have a {platform} file. ",
-        "nl": f"Volg de download instructies en kies het bestand dat u opgeslagen heeft op uw apparaat. Als u geen {platform} bestand heeft klik dan op “Overslaan” rechts onder."
+        "nl": descriptionString
     })
 
     return props.PropsUIPromptFileInput(description, extensions)
@@ -136,7 +164,7 @@ def prompt_consent(list_with_df_with_chats):
         "nl": "Inhoud zip bestand"
     })
 
-    table_list = [props.PropsUIPromptConsentFormTable(f"zip_content: {index}", table_title, df) 
+    table_list = [props.PropsUIPromptConsentFormTable(f"zip_content: {index}", table_title, df)
         for index, df in enumerate(list_with_df_with_chats)]
 
     return props.PropsUIPromptConsentForm(table_list, [])
